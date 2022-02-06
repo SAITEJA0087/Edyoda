5259631e809bce67e6267d83fc9be09c79b28a50
SAITEJA0087 <71368463+SAITEJA0087@users.noreply.github.com>
2022-02-06 23:08:17 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Sample_list=[4,5,2,9]
+def sqr(x):
+    return(x**2)
+print('square of elements of givenlist using map() is',list(map(sqr,Sample_list)))
+
+
+# In[ ]:
+
+
+
+
